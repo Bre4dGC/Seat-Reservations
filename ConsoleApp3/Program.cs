0d72c5d2f0ef4a2c9e1e8bd01ed2fccd294a9b1e
Bre4dGC <glebbreadc@gmail.com>
2024-03-04 01:31:58 +0300

    second commit

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,29 +10,30 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+            Console.Title = "Администрирование кафе";
             int[] table = { 5, 3, 5, 1, 4, 2 };
             bool isOpen = true;
 
             while (isOpen)
             {
-                Console.SetCursorPosition(25, 11);
-                Console.WriteLine("1 - Забронированить");
-
-                Console.SetCursorPosition(25, 12);
-                Console.WriteLine("2 - Освободить место");
-
-                Console.SetCursorPosition(25, 13);
-                Console.WriteLine("3 - Выйти");
-
-                Console.SetCursorPosition(7, 10);
+                Console.SetCursorPosition(7, 11);
                 Console.WriteLine("Столы");
                 for (int i = 0; i < table.Length; i++)
                 {
                     Console.WriteLine($"|{(i + 1)} - свободно {table[i]} мест|");
                 }
-                
-                Console.SetCursorPosition(10, 0);
-                Console.WriteLine("Администрирование кафе");
+
+                Console.SetCursorPosition(25, 12);
+                Console.WriteLine("1 - Забронированить");
+
+                Console.SetCursorPosition(25, 13);
+                Console.WriteLine("2 - Освободить место");
+
+                Console.SetCursorPosition(25, 14);
+                Console.WriteLine("3 - Выйти");
+
+                Console.SetCursorPosition(15, 0);
+                Console.WriteLine("|Администрирование кафе|");
 
                 Console.Write("\nВведите номер команды: ");
 
@@ -41,6 +42,13 @@ namespace ConsoleApp3
                     case 1:
                         Console.Write("\nВведите стол: ");
                         int userTable = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                        if (table.Length < userTable || userTable < 0)
+                        {
+                            Console.Write("Некорректное число"); Console.ReadKey(); Console.Clear();
+                            break;
+                        }
+
                         Console.Write("\nВведите количество мест для брони: ");
                         int userPlase = Convert.ToInt32(Console.ReadLine());
 
@@ -49,20 +57,29 @@ namespace ConsoleApp3
                             Console.Write("Недостаточно мест"); Console.ReadKey(); Console.Clear();
                             break;
                         }
+
                         table[userTable] -= userPlase; Console.Clear();
                         break;
 
                     case 2:
                         Console.Write("\nВведите стол: ");
                         userTable = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                        if (table.Length < userTable || userTable < 0)
+                        {
+                            Console.Write("Некорректное число"); Console.ReadKey(); Console.Clear();
+                            break;
+                        }
+
                         Console.Write("\nВведите количество освободившихся мест: ");
                         userPlase = Convert.ToInt32(Console.ReadLine());
 
-                        if (table[userTable] > userPlase || userPlase < 0)
+                        if (table[userTable] < userPlase || userPlase < 0)
                         {
-                            Console.Write($"Максимум может быть только {table[userTable]} мест"); Console.ReadKey(); Console.Clear();
+                            Console.Write("Некорректное число"); Console.ReadKey(); Console.Clear();
                             break;
                         }
+
                         table[userTable] += userPlase; Console.Clear();
                         break;
 
